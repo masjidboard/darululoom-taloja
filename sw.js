2a2ba2a1f687a22f92f587e9f97180eea2bd6c83
masjidboard <masjidboard820@gmail.com>
2026-06-07 20:03:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,17 @@
-const CACHE = 'darululoom-v1';
-const FILES = [
-  '/',
-  '/index.html',
-  'https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu:wght@400;600;700&family=Amiri:wght@400;700&display=swap'
-];
+const CACHE = 'darululoom-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {}))
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
