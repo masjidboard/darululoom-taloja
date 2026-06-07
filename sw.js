5be9081351c6d38e455acc5dadd57b7642c9ec1d
masjidboard <masjidboard820@gmail.com>
2026-06-07 20:29:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-const CACHE = 'darululoom-v2';
+const CACHE = 'darululoom-v3';
+const BASE = '/darululoom-taloja/';
 
 self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.add(BASE)).catch(()=>{})
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => 
-      Promise.all(keys.map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match(BASE)))
+  );
 });
